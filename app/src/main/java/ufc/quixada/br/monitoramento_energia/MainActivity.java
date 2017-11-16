721d48eb289ce90da8e86ebfcc17e4c390ffbaf6
Iarlen <iarlensilva775@gmail.com>
2017-11-16 00:37:33 -0300

    criando projeto

---
+++ app/src/main/java/ufc/quixada/br/monitoramento_energia/MainActivity.java
@@ -0,0 +1,86 @@
+package ufc.quixada.br.monitoramento_energia;
+
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+import com.androidplot.util.PixelUtils;
+import com.androidplot.xy.CatmullRomInterpolator;
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYGraphWidget;
+import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.XYSeries;
+
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.ParsePosition;
+import java.util.Arrays;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private XYPlot plot;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // inicializar referência do XYPlot
+        plot = (XYPlot) findViewById(R.id.plot);
+        final String[] mMonths = new String[]{
+                "Jan", "Fev", "Mar", "Abr", "Mai", "Jun",
+                "Jul", "Ago", "Set", "Out", "Nov", "Dez"
+        };
+        Number[] series1Numbers =  {2000, 2500, 2700, 3000, 2800, 3500, 3700, 3800 };
+        Number[] series2Numbers = {2200, 2700, 2900, 2800, 2600, 3000, 3300, 3400 };
+
+        XYSeries series1 = new SimpleXYSeries(
+                Arrays.asList(series1Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Corrente");
+        XYSeries series2 = new SimpleXYSeries(
+                Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Tensao");
+
+        LineAndPointFormatter series1Format = new LineAndPointFormatter(Color.RED, Color.BLUE, Color.GREEN, null);
+
+        LineAndPointFormatter series2Format = new LineAndPointFormatter(Color.YELLOW, Color.GRAY, Color.BLACK, null);
+
+        series2Format.getLinePaint().setPathEffect(new DashPathEffect(new float[]{
+
+                // always use DP when specifying pixel sizes, to keep things consistent across devices:
+                PixelUtils.dpToPix(20),
+                PixelUtils.dpToPix(15)}, 0));
+
+        series1Format.setInterpolationParams(
+                new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
+
+        series2Format.setInterpolationParams(
+                new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
+
+        plot.addSeries(series1, series1Format);
+
+
+        plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new Format() {
+            @Override
+            public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
+                return new StringBuffer(mMonths[((Number) obj).intValue()]);
+            }
+
+            @Override
+            public Object parseObject(String source, ParsePosition pos) {
+                return null;
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
